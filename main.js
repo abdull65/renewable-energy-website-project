837282510f2a64217d9dd7d667f2f3d7411f3f4e
Abdullahi Mohammed <odanwu211@gmail.com>
2023-05-12 07:19:29 +0100

    update/adding responsiveness to services content

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("open_nav");
 });
 // Sticky navigation
-const indexSectionElEl = document.querySelector("#hero_section");
+const indexSectionElEl = document.querySelector("header");
+const heroSectionEl = document.querySelector(".hero-section");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
@@ -14,10 +15,12 @@ const obs = new IntersectionObserver(
 
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
+      heroSectionEl.style.marginTop = "100px";
     }
 
     if (ent.isIntersecting) {
       document.body.classList.remove("sticky");
+      heroSectionEl.style.marginTop = "0";
     }
   },
   {
@@ -31,10 +34,9 @@ obs.observe(indexSectionElEl);
 
 // making service section text responsive
 const readMoreBtns = document.querySelectorAll(".service-typedtext_box");
+const readMoreBtnEl = document.querySelector(".read-more");
 readMoreBtns.forEach((readMore) => {
   readMore.addEventListener("click", function () {
     readMore.classList.toggle("show-text");
-    const readMoreBtnEl = document.querySelector(".read-more");
-    readMoreBtnEl.textContent = "see less";
   });
 });
